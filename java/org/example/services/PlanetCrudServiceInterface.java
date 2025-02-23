77c3b28520a9ba74e16a864dddb6b39b2eb533bd
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-23 20:09:14 +0100

    Update PlanetCrudServiceInterface.java

--- java/org/example/services/PlanetCrudServiceInterface.java
+++ java/org/example/services/PlanetCrudServiceInterface.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface PlanetCrudServiceInterface {
     void createPlanet(Planet planet);
+    
     Planet getPlanetById(String id);
+    
     List<Planet> getAllPlanets();
+    
     void updatePlanet(Planet planet);
+    
     void deletePlanet(String id);
 }
