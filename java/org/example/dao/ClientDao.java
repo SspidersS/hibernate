4c97c390e2e43a0f5a7b5d94dd12f37d01b959aa
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-23 20:08:31 +0100

    Update ClientDao.java

--- java/org/example/dao/ClientDao.java
+++ java/org/example/dao/ClientDao.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface ClientDao {
     void save(Client client);
+    
     Client getById(Long id);
+    
     List<Client> getAll();
+    
     void update(Client client);
+    
     void delete(Long id);
 }
