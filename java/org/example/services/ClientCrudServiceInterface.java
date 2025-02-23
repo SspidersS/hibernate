181de66b16f5484197191cabe30ae546470481de
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-23 20:09:05 +0100

    Update ClientCrudServiceInterface.java

--- java/org/example/services/ClientCrudServiceInterface.java
+++ java/org/example/services/ClientCrudServiceInterface.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface ClientCrudServiceInterface {
     void createClient(Client client);
+    
     Client getClientById(Long id);
+    
     List<Client> getAllClients();
+    
     void updateClient(Client client);
+    
     void deleteClient(Long id);
 }
